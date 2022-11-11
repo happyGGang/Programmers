2a16c6954e03a338612d538db4844246aecc2c9a
happyGGang <joy.gang@dxworks.kr>
2022-11-11 22:46:19 +0900

    [Lv.0] 배열의 평균값

---
+++ Lv.0/배열의 평균값.js
@@ -0,0 +1,10 @@
+// 정수 배열 numbers가 매개변수로 주어집니다. numbers의 원소의 평균값을 return하도록 solution 함수를 완성해주세요.
+
+// 0 ≤ numbers의 원소 ≤ 1,000
+// 1 ≤ numbers의 길이 ≤ 100
+// 정답의 소수 부분이 .0
+
+function solution(numbers) {
+  var answer = numbers.reduce((a, b) => a + b, 0) / numbers.length;
+  return answer;
+}
