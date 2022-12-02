ec21791d1815ef58657f248ec6d95d0d6cedab2d
ykkang <ykkang@ocube.co.kr>
2022-12-02 23:28:41 +0900

    [Lv.0] 숨어있는 숫자의 덧셈(1)

---
+++ Lv.0/숨어있는 숫자의 덧셈(1).js
@@ -0,0 +1,14 @@
+// 문자열 my_string이 매개변수로 주어집니다. my_string안의 모든 자연수들의 합을 return하도록 solution 함수를 완성해주세요.
+
+// 1 ≤ my_string의 길이 ≤ 1,000
+// my_string은 소문자, 대문자 그리고 한자리 자연수로만 구성되어있습니다.
+
+function solution(my_string) {
+  const num = my_string.split("");
+  let result = 0;
+
+  for (let i = 0; i < num.length; i++) {
+    if (Number(num[i])) result += Number(num[i]);
+  }
+  return result;
+}
